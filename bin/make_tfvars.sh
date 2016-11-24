8183a6c0b0211253ddbe43566aa85923ac949bb1
Colin Simmons <colin.r.simmons@gmail.com>
2016-11-24 16:02:15 +0000

    var names

--- bin/make_tfvars.sh
+++ bin/make_tfvars.sh
@@ -3,6 +3,6 @@
 cat >terraform/terraform.tfvars <<EOF
 boshers = $BOSHERS
 ssl_cert_arn="$SSL_CERT_ARN"
-ci_dns_zone="$CI_DNS_ZONE"
+ci_dns_zone_id="$CI_DNS_ZONE_ID"
 ci_hostname="$CI_HOSTNAME"
 EOF
