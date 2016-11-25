5a52dd45bf39971ee7a15746c20326ca636108d2
Colin Simmons <colin.r.simmons@gmail.com>
2016-11-25 11:49:07 +0000

    consolidated into one task

--- pipeline/tasks/bosh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./bin/make_manifest_bosh-init.sh
-bosh-init deploy bosh-director.yml
-bosh target $(terraform output eip)
-./bin/make_cloud_config.sh
-bosh update cloud-config aws-cloud.yml
--- pipeline/tasks/concourse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./bin/make_manifest_concourse.sh
