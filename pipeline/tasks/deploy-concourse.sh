a6d1f2d3bf92d5bf314577e00f6af18855d48054
Colin Simmons <colin.r.simmons@gmail.com>
2016-11-28 19:17:42 +0000

    versions are hard

--- pipeline/tasks/deploy-concourse.sh
+++ pipeline/tasks/deploy-concourse.sh
@@ -3,6 +3,9 @@
 ./bin/make_tfvars.sh
 make infra
 
+# I'm so sorry...
+mv ../private_key/${AWS_KEYPAIR_KEY_NAME}{-$(cat ../private_key/version),}.pem
+
 ./bin/make_manifest_bosh-init.sh
 bosh-init deploy bosh-director.yml || make destroy
 bosh target $(terraform output eip)
